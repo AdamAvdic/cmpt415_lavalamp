8a512299ea4ac1ca518d60cd0daec896902b5b08
Adam Avdic <adamavdic@gmail.com>
2024-11-24 21:20:55 -0800

    db fake migrations

--- base/models.py
+++ base/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class HuggingFaceRefineWeb(models.Model):
-    content = models.TextField()
+    content = models.TextField(primary_key=True)
     url = models.TextField(max_length=200)  # Field for storing the URL
     timestamp = models.TextField()    # Field for storing the timestamp
 
     class Meta:
-        db_table = 'huggingface-refineweb'  # Match the table name in your database
+        db_table = 'base_huggingfacerefineweb'  # Match the table name in your database
 
     def __str__(self):
         return self.content
